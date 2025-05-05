558913ac2f3e2bad0370d53a23bedac407d246c7
Aakash <aakash.yogabalu@gmail.com>
2025-05-05 19:20:21 -0300

    Adding Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './css/App.css'
 import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Fav from './pages/Fav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h>Hellooo</h>
       <Routes>
         <Route path= "/" element={<Home />}/>
+        <Route path= "/favorites" element={<Fav />}/>
       </Routes>
     </>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home(){
+    return(
+        <>
+            <h1>Hello</h1>
+            <div>
+                <p>Helllloooooo</p>
+            </div>
+        </>
+    )
+}
+
+export default Home;
