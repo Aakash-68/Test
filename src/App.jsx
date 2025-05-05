81f9d96c3f6c947923b0de8f2c4afa25347a9bbf
Aakash <aakash.yogabalu@gmail.com>
2025-05-05 08:24:08 -0300

    Pushing the default setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './css/App.css'
+import {Routes, Route} from "react-router-dom"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path= "/" element={<Home />}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
